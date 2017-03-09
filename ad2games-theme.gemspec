d888906128a0f961f9a3fcaaadd2843d664870a2
Matthew Correia <mco@ad2games.com>
2017-03-09 13:47:48 +0100

    add support for filter-toolbar (#3)

--- ad2games-theme.gemspec
+++ ad2games-theme.gemspec
@@ -3,7 +3,7 @@ $LOAD_PATH.push File.expand_path('../lib', __FILE__)
 
 Gem::Specification.new do |s|
   s.name      = 'ad2games-theme'
-  s.version   = 0.72
+  s.version   = 0.73
   s.platform  = Gem::Platform::RUBY
   s.authors   = ['ad2games']
   s.email     = ['developers@ad2games.com']
--- app/assets/javascripts/ad2games-theme.js
+++ app/assets/javascripts/ad2games-theme.js
@@ -1,10 +1,4 @@
 $(function() {
   // add active class to top-level ancestor nav item
   $('.navbar .active').parents('li').addClass('active');
-
-  // trigger change event on toggle buttons (containing hidden checkbox)
-  $('.toolbar .checkbox input').change(function() {
-    return $(this).parents('li.btn').toggleClass('active', this.checked);
-  });
-  $('.toolbar .checkbox input').trigger('change');
 });
